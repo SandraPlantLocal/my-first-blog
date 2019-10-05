e6cb6c3f040406574f36aeaef0b66d9eeaf15d0d
SandraPlantLocal <sandra@plantlocal.net>
2019-10-05 12:21:33 +0200

    My Django Girls app, first commit

---
+++ py_code.py
@@ -0,0 +1,19 @@
+
+test_dict = {
+    'event': 'django_girls',
+    'date': 'today',
+    'participants':50}
+
+print(test_dict['event'])
+print (test_dict)
+print ('test_dict')
+
+test_dict['location'] = 'Berlin'
+
+print (test_dict)
+
+def sum(a, b):
+    return a+b
+c = sum(5, 9)
+print(c)
+print(sum(5, 10))
